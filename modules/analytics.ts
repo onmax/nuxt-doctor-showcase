05f62f341128134bd28c651bca8ba3ec2e677a24
onmax <maximogarciamtnez@gmail.com>
2026-02-01 08:59:26 +0100

    use new DoctorCheck schema fields

--- modules/analytics.ts
+++ modules/analytics.ts
@@ -13,6 +13,7 @@ export default defineNuxtModule({
     nuxt.hook('doctor:check', (ctx: any) => {
       if (!options.trackingId) {
         ctx.addCheck({
+          id: 'ANALYTICS_MISSING_ID',
           name: 'Analytics',
           status: 'error',
           message: 'missing required config',
@@ -20,14 +21,18 @@ export default defineNuxtModule({
           details: [
             'trackingId is required but not set',
             'Add analytics.trackingId to nuxt.config.ts'
-          ]
+          ],
+          suggestion: 'Add analytics: { trackingId: "UA-XXXXX-Y" } to nuxt.config.ts',
+          url: 'https://analytics.google.com/',
+          data: { configKey: 'analytics.trackingId' }
         })
       } else {
         ctx.addCheck({
           name: 'Analytics',
-          status: 'success', 
+          status: 'success',
           message: `configured (${options.trackingId})`,
-          source: 'analytics'
+          source: 'analytics',
+          data: { trackingId: options.trackingId }
         })
       }
     })
